bb1578cf0ce06ca8a925a45705a2626cb59b539c
Vincenzo Gavioli <vgavioli@e3r8p8.42roma.it>
2023-03-22 10:44:06 +0100

    prompt: fix weird behaviour in displaying path. Too many free?

--- main/main.c
+++ main/main.c
@@ -13,29 +13,54 @@
 //I think it's quite an appropriate name for the process of splitting and
 //tokenizing the input. And it's quite some badass music.
 //If you don't agree, feel free to go and f##k off :)
+static char *remove_home(char *str, char *home)
+{
+	char 	*new;
+	int		i;
+	int		j;
+
+	new = ft_calloc(sizeof(char), ft_strlen(str)  - ft_strlen(home) + 2);
+	if (!new)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == home[i])
+			i++;
+		else
+			break ;
+	}
+	if (str[i])
+		i++;
+	while (str[i])
+		new[j++] = str[i++];
+	return (new);
+}
+
 static char	*our_prompt(t_hellmini *shell, char *str)
 {
 	char	*buff;
-	char	*path;
+	char	*home_path;
 	char	*new_path;
 
-	path = exp_tkn("HOME", shell->env);
+	home_path = exp_tkn("HOME", shell->env);
 	buff = ft_calloc(sizeof(char), 2);
 	buff[0] = '[';
 	buff = ft_strjoin_free(buff, ft_itoa(get_ecode()), 1, 1);
 	buff = ft_strjoin_free(buff, "] "BYEL, 1, 0);
 	buff = ft_strjoin_free(buff, exp_tkn("USER", shell->env), 1, 1);
 	buff = ft_strjoin_free(buff, PROMPT, 1, 0);
-	if (!ft_strncmp(str, path, ft_strlen(path)))
+	if (!ft_strncmp(str, home_path, ft_strlen(home_path)))
 	{
-		new_path = ft_strtrim(str, path);
 		buff = ft_strjoin_free(buff, "~/", 1, 0);
+		new_path = remove_home(str, home_path);
 		buff = ft_strjoin_free(buff, new_path, 1, 1);
 		free(str);
 	}
 	else
 		buff = ft_strjoin_free(buff, str, 1, 1);
-	free(path);
+	free(home_path);
 	buff = ft_strjoin_free(buff, WHITE"$ ", 1, 0);
 	return (buff);
 }
