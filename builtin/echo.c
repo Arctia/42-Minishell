ae34df5ca0e566786a47a49edc0fda6bdc6c28cf
arctia <arctia.games@gmx.com>
2023-03-22 13:31:38 +0100

    echo: fix back to mac

--- builtin/echo.c
+++ builtin/echo.c
@@ -1,5 +1,32 @@
 #include "./../global.h"
 
+static int	skip_arguments(t_command *cmd, int	*flag)
+{
+	int	n;
+	int	i;
+
+	n = 1;
+	while (cmd->arguments && cmd->arguments[n])
+	{
+		if (cmd->arguments[n][0] == '-')
+		{
+			i = 1;
+			while (cmd->arguments[n][i] == 'n')
+				i++;
+			if (i > 1 && cmd->arguments[n][i] == '\0')
+			{
+				n++;
+				*flag = 1;
+			}
+			else
+				break ;
+		}
+		else
+			break ;
+	}
+	return (n);
+}
+
 int	echo(t_command *cmd)
 {
 	int		n_flag;
@@ -7,9 +34,9 @@ int	echo(t_command *cmd)
 	int		i;
 
 	n_flag = 0;
-	n = 0;
-	while (cmd->arguments[++n] && !ft_strncmp(cmd->arguments[n], "-n", 3))
-		n_flag = 1;
+	n = skip_arguments(cmd, &n_flag);
+	//while (cmd->arguments[++n] && !ft_strncmp(cmd->arguments[n], "-n", 3))
+	//	n_flag = 1;
 	while (cmd->arguments[n])
 	{
 		if (cmd->arguments[n + 1])
