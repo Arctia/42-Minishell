678d3359f17e8d2c2ab9b8d085f5932932c28804
Mauro Bardetta <mbardett@e3r10p4.42roma.it>
2023-03-22 23:02:16 +0100

    final env norm, printf

--- builtin/env.c
+++ builtin/env.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbardett <mbardett@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 22:42:43 by mbardett          #+#    #+#             */
+/*   Updated: 2023/03/22 22:42:43 by mbardett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../global.h"
 
 void	ft_putendl_fd(char *s, int fd)
@@ -6,32 +18,32 @@ void	ft_putendl_fd(char *s, int fd)
 	ft_putchar_fd('\n', fd);
 }
 
-static int  ft_strchr_len(char *str, char c)
+static int	ft_strchr_len(char *str, char c)
 {
-  int  i;
+	int		i;
 
-  i = 0;
-  while (str[i])
-  {
-    if (str[i] == c)
-      break ;
-    i++;
-  }
-  if (i >= (int) ft_strlen(str))
-    return (-1);
-  return (i);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			break ;
+		i++;
+	}
+	if (i >= (int) ft_strlen(str))
+		return (-1);
+	return (i);
 }
 
-int  env(t_command *cmd)
+int	env(t_command *cmd)
 {
-  int  i;
+	int		i;
 
-  i = 0;
-  while (cmd->shell->env[i])
-  {
-    if (!(ft_strchr_len(cmd->shell->env[i], '=') == -1))
-      ft_putendl_fd(cmd->shell->env[i], 1);
-    i++;
-  }
-  return (0);
+	i = 0;
+	while (cmd->shell->env[i])
+	{
+		if (!(ft_strchr_len(cmd->shell->env[i], '=') == -1))
+			ft_putendl_fd(cmd->shell->env[i], 1);
+		i++;
+	}
+	return (0);
 }
